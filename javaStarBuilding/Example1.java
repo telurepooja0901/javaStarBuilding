ea9e892314c73701bf4bdcff6b75e49f6e82b579
telurepooja0901 <telurepooja0901@gmail.com>
2024-06-17 16:15:45 +0530

    first commit

---
+++ javaStarBuilding/Example1.java
@@ -0,0 +1,19 @@
+package com.javaStarBuilding;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+
+		for(int i = 0 ; i < 6 ; i++)
+		{
+			
+			for(int j= 0 ; j < i ; j++)
+			{
+				System.out.print("*");
+			
+			}
+			
+			System.out.println();
+		}
+	}	
+}
---
+++ javaStarBuilding/Example3.java
@@ -0,0 +1,20 @@
+package com.javaStarBuilding;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		
+		int i, j;
+
+		for(i = 1; i < 5; i ++)
+		{
+			for(j = 1; j <= i; j --)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+				
+		}
+	}	
+}
